46476ba97ff3a342ca22f9c66f6cfc43e6cd44ca
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2022-11-13 15:19:09 +0600

    final

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,28 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+const Stack = createStackNavigator();
+import { NavigationContainer } from "@react-navigation/native";
 
 const Home = () => {
-  return (
-    <View
-      style={{
-        backgroundColor: "#022",
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-      }}
-    >
-      <Text style={{ color: "white", fontSize: 20, alignSelf: "center" }}>
-        Home Screen
-      </Text>
-    </View>
-  );
+  return; // <View
+  //   style={{
+  //     backgroundColor: "#637",
+  //     flex: 1,
+  //     alignItems: "center",
+  //     justifyContent: "center"
+  //   }}
+  // >
+  //   <Text style={{ color: "white", fontSize: 20, alignSelf: "center" }}>
+  //     Home Screen
+  //   </Text>
+  // </View>
+
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={Home} />
+    <Stack.Screen name="Notifications" component={Notifications} />
+    <Stack.Screen name="Profile" component={Profile} />
+    <Stack.Screen name="Settings" component={Settings} />
+  </Stack.Navigator>;
 };
 
 export default Home;
