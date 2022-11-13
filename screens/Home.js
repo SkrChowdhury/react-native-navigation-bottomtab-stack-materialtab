766ec399691e1d8904e1c621ffccc0b15e7b9b09
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2022-11-13 12:46:15 +0600

    navigator added

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 const Home = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home Screen</Text>
     </View>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 const Profile = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Profile Screen</Text>
     </View>
   );
 };
 
 export default Profile;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
